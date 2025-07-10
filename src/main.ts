f8483dc6140a1c9b358c5f297b349ffde460d769
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-07-10 14:27:11 +0300

    eliminated cors opions

--- src/main.ts
+++ src/main.ts
@@ -11,31 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      process.env.ORIGIN,
-      process.env.ORIGIN_RENDER,
-      process.env.ORIGIN_REACT,
-      'https://react-learn-flax.vercel.app',
-      'https://angular-refresh.vercel.app',
-
-      'https://react-learn-3fn37hc78-adrianilie01s-projects.vercel.app',
-      'http://localhost:4200',
-      'http://localhost:5173',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-
     exposedHeaders: ['x-forwarded-for', 'set-cookie'],
-
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'X-Requested-With',
-      'Accept',
-      'Origin',
-      'Access-Control-Allow-Origin',
-      'Set-Cookie',
-    ],
-
     credentials: true,
   });
 
