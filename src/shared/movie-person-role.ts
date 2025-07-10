9958f3b3b20c316e5da47c572163135c60c59eac
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-07-10 11:04:10 +0300

    added: new roles for Movie Persons

--- src/shared/movie-person-role.ts
+++ src/shared/movie-person-role.ts
@@ -1,5 +1,12 @@
 export enum MoviePersonRole {
   Actor = 'actor',
   Director = 'director',
-  Producer = 'producer'
+  Producer = 'producer',
+  Writer = 'writer',
+  Cinematographer = 'cinematographer',
+  Editor = 'editor',
+  Composer = 'composer',
+  ProductionDesigner = 'production_designer',
+  CostumeDesigner = 'costume_designer',
+  SoundDesigner = 'sound_designer'
 }
